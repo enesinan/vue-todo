e402f6b605c5e049b18102b522c578a3f7347ec8
enes-dev <inane064@gmail.com>
2021-04-13 12:44:25 +0300

    Added App.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+<AppHeader />
+<div class="w-full flex">
+<DcHeros />
+</div>
+</template>
+
+<script>
+import AppHeader from "./assets/components/AppHeader";
+import DcHeros from './assets/components/DcHeros.vue';
+export default {
+  components: { AppHeader, DcHeros},
+ 
+};
+</script>
+
+<style>
+
+</style>
