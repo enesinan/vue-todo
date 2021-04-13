ac2a67bac1c8a71cfe0e78072726837b584092e5
enes-dev <inane064@gmail.com>
2021-04-13 12:46:44 +0300

    Added AppHeader file

---
+++ src/assets/components/AppHeader.vue
@@ -0,0 +1,17 @@
+<template>
+
+<nav class="w-full bg-gradient-to-r from-blue-600 to-blue-400 text-white px-8 py-4">
+    <a>TO DO</a>
+</nav>
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
